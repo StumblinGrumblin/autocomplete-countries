940517b7fdfdf9b168a23bab4343d7eeddfd1dd7
StumblinGrumblin <gary@sweetlabs.com>
2015-04-07 08:08:51 -0700

    Basic page style added

--- myPage/js/app.js
+++ myPage/js/app.js
@@ -1,49 +1,89 @@
 var csvData;
+var results;
+function findCountry(cName) {
+  return $.grep(csvData, function (n, i) {
+    return n.name == cName;
+  });
+};
 $(document).ready(function() {
-  $.ajax({
-    type: "GET",
-    url: "countries.csv",
-    dataType: "text",
-    success: function (result) {
-      processCSV(result);
-      $.each(csvData, function (i, name) {
-        console.log("Index #:" + i + ": " + name[1]);
-        $('#selected ul').append("<li><img src='images/flags/"
-          + name[3]
-          + "' alt='flag'/>"
-          + "<span><strong>"
-          + name[1]
-          + "</strong></span></br>"
-          + name[0]
-          + "</li>"
-          );
-      });
+  var people = ['Peter Bishop', 'Nicholas Brody', 'Gregory House', 'Hank Lawson', 'Tyrion Lannister', 'Nucky Thompson'];
+  var cache = {};
+  var drew = false;
+
+  $.get('countries.csv', function (data) {
+    csvJSON(data);
+  });
+
+  $('#selectCountry').on("keyup", function (event) {
+    var query = $("#selectCountry").val()
+    if ($('#selectCountry').val().length > 0) {
+
+      if (query in cache) {
+        results = cache[query];
+      } else {
+        results = $.grep(csvData, function (item) {
+          return item.name == RegExp(query, "i");
+        });
+        cache[query] = results;
+      }
+
+      if (drew == false) {
+        $('#selectCountry').after('<ul id="res"></ul>');
+        drew = true;
+
+        $("#res").on("click", "li", function () {
+          $("#selectCountry").val($(this).text());
+          $("#res").empty();
+        });
+      } else {
+        $("#res").empty();
+      }
+      for (name in results) {
+        $("#res").append("<li>" + results[name].name + "</li>" );
+      }
+    } else if (drew) {
+      $("#res").empty();
     }
 
   });
   $('form').submit(function (event) {
     console.log("Handler for .submit() called.");
+    $('#selected ul').append("<li><img src='images/flags/"
+      + csvData[0].imgLG
+      + "' alt='flag'/>"
+      + "<div><p><strong>"
+      + csvData[0].abbr
+      + "</strong></p>"
+      + csvData[0].name
+      + "</div></li>"
+      );
     event.preventDefault();
     $(this).find("input[type=text]").val("");
   });
+
+
+
 })
 
-function processCSV(text) {
+function csvJSON(csv){
+  var
+    headers = ["name", "abbr", "imgSM", "imgLG"],
+    lines = csv.split("\n"),
+    result = [];
+  for(var i = 0; i < lines.length; i++){
     var
-      allTextLines = text.split(/\r\n|\n/),
-      columns = 4,
-      lines = [];
-
-    for (var i=0; i < allTextLines.length; i++) {
-        var data = allTextLines[i].split(',');
-        if (data.length == columns) {
-
-            var tarr = [];
-            for (var j=0; j < columns; j++) {
-                tarr.push(data[j]);
-            }
-            lines.push(tarr);
-        }
+      currentline = lines[i].split(","),
+      key = currentline[0].toString(),
+      obj = {},
+      obj2 = {};
+    for(var j = 0; j < headers.length; j++){
+      obj[headers[j]] = currentline[j];
     }
-    csvData = lines;
+
+    result.push(obj);
+
+  }
+  //return result; //JavaScript object
+  return csvData = result; //JSON
+
 }
